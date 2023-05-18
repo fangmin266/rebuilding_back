aa7b2b00489abb47e8a8ea934f8f6978321e0dcc
fangmin26 <fangmin_@naver.com>
2023-05-18 21:47:15 +0900

    nginx

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,16 @@ async function bootstrap() {
     .addTag('clound funding rebuilding backend project')
     .build();
 
-  const httpsOptions = {
-    key: fs.readFileSync('./cert/key.pem'),
-    cert: fs.readFileSync('./cert/cert.pem'), //에러 생김 : 파일 위치 문제였음 ^^;
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  // const httpsOptions = {
+  //   key: fs.readFileSync('./cert/key.pem'),
+  //   cert: fs.readFileSync('./cert/cert.pem'), //에러 생김 : 파일 위치 문제였음 ^^;
+  // };
+  // const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
-  await app.listen(443);
+  await app.listen(3600);
 }
 bootstrap();
